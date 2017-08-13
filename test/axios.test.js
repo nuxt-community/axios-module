e11e8a6fa9e8a06c6a61c206965d5e52772845ab
Pooya Parsa <pooya@pi0.ir>
2017-08-13 16:05:35 +0430

    fix tests

--- test/axios.test.js
+++ test/axios.test.js
@@ -9,7 +9,6 @@ describe('axios module', () => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
         nuxt = new Nuxt({ srcDir: __dirname + '/fixture', dev: false })
         let builder = new Builder(nuxt)
-        builder.webpackStats = false
         await builder.build()
     })
 
