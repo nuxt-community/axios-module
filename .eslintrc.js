4096f83530f850614391f81e433d04e844520b64
Pooya Parsa <pooya@pi0.ir>
2017-08-13 16:40:55 +0430

    working eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  // parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
   env: {
     browser: true,
-    node: true
+    node: true,
+    jest: true
   },
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
-    'html'
+    'jest',
+    'vue'
   ],
-  // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
+    // Allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
+    // Allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
+    // Allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // do not allow console.logs etc...
+    // Do not allow console.logs etc...
     'no-console': 2
   },
-  globals: {}
+  globals: {
+    'jest/globals': true,
+    jasmine: true
+  }
 }
--- test/axios.test.js
+++ test/axios.test.js
@@ -1,18 +1,19 @@
 const { Nuxt, Builder } = require('nuxt')
+const { resolve } = require('path')
 
 process.env.NODE_ENV = 'production'
 
 describe('axios module', () => {
-    let nuxt
+  let nuxt
 
-    beforeAll(async () => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
-        nuxt = new Nuxt({ srcDir: __dirname + '/fixture', dev: false })
-        let builder = new Builder(nuxt)
-        await builder.build()
-    })
+  beforeAll(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+    nuxt = new Nuxt({ srcDir: resolve(__dirname, 'fixture'), dev: false })
+    let builder = new Builder(nuxt)
+    await builder.build()
+  })
 
-    test('test', () => {
-        expect(nuxt.options.dev).toBe(false)
-    })
+  test('test', () => {
+    expect(nuxt.options.dev).toBe(false)
+  })
 })
