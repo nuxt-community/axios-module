c63d5ef139a9db558cce5e17fa04a822b1f35c5a
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-18 18:58:35 -0300

    test(module): increase coverage (#213)

--- test/axios.test.js
+++ test/axios.test.js
@@ -2,39 +2,34 @@ jest.setTimeout(60000)
 
 const { Nuxt, Builder } = require('nuxt-edge')
 const axios = require('axios')
+
 const config = require('./fixture/nuxt.config')
+
+let nuxt, addTemplate
 
 const url = path => `http://localhost:3000${path}`
 
-describe('axios module', () => {
-  let nuxt
-  let addTemplate
+const setupNuxt = async (config) => {
+  nuxt = new Nuxt(config)
 
-  beforeAll(async () => {
-    nuxt = new Nuxt(config)
+  // Spy addTemplate
+  addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(
+    nuxt.moduleContainer.addTemplate
+  )
 
-    // Spy addTemplate
-    addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(
-      nuxt.moduleContainer.addTemplate
-    )
+  const build = new Builder(nuxt)
 
-    await new Builder(nuxt).build()
-    await nuxt.listen(3000)
-  })
+  await build.validatePages()
+  await build.generateRoutesAndFiles()
+  await nuxt.listen(3000)
+}
 
-  afterAll(async () => {
-    await nuxt.close()
-  })
-
+const testSuite = () => {
   test('baseURL', () => {
     expect(addTemplate).toBeDefined()
-    const call = addTemplate.mock.calls.find(args =>
-      args[0].src.includes('plugin.js')
-    )
+    const call = addTemplate.mock.calls.find(args => args[0].src.includes('plugin.js'))
     const options = call[0].options
-    expect(options.baseURL.toString()).toBe(
-      `http://localhost:3000/test_api`
-    )
+    expect(options.baseURL.toString()).toBe('http://localhost:3000/test_api')
     expect(options.browserBaseURL.toString()).toBe('/test_api')
   })
 
@@ -60,12 +55,11 @@ describe('axios module', () => {
   })
 
   test('ssr', async () => {
-    const makeReq = login =>
-      axios
-        .get(url('/ssr' + (login ? '?login' : '')))
-        .then(r => r.data)
-        .then(h => /session-[0-9]+/.exec(h))
-        .then(m => (m && m[0] ? m[0] : null))
+    const makeReq = login => axios
+      .get(url('/ssr' + (login ? '?login' : '')))
+      .then(r => r.data)
+      .then(h => /session-[0-9]+/.exec(h))
+      .then(m => (m && m[0] ? m[0] : null))
 
     const a = await makeReq()
     const b = await makeReq(true)
@@ -80,15 +74,96 @@ describe('axios module', () => {
   })
 
   test('ssr no brotli', async () => {
-    const makeReq = login =>
-      axios
-        .get(url('/ssr' + (login ? '?login' : '')))
-        .then(r => r.data)
-        .then(h => /encoding-\$(.*)\$/.exec(h))
-        .then(m => (m && m[1] ? m[1] : null))
+    const makeReq = login => axios
+      .get(url('/ssr' + (login ? '?login' : '')))
+      .then(r => r.data)
+      .then(h => /encoding-\$(.*)\$/.exec(h))
+      .then(m => (m && m[1] ? m[1] : null))
 
     const result = await makeReq()
 
     expect(result).toBe('gzip, deflate')
+  })
+}
+
+describe('module', () => {
+  beforeAll(async () => {
+    nuxt = new Nuxt(config)
+
+    // Spy addTemplate
+    addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(
+      nuxt.moduleContainer.addTemplate
+    )
+
+    await new Builder(nuxt).build()
+    await nuxt.listen(3000)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  testSuite()
+})
+
+describe('other options', () => {
+  beforeAll(async () => {
+    config.axios = {
+      prefix: '/test_api',
+      proxy: {},
+      credentials: true,
+      https: true,
+      retry: false
+    }
+
+    await setupNuxt(config)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  testSuite()
+})
+
+describe('browserBaseURL', () => {
+  beforeAll(async () => {
+    config.axios = {
+      browserBaseURL: '/test_api'
+    }
+
+    await setupNuxt(config)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('custom', () => {
+    expect(addTemplate).toBeDefined()
+    const call = addTemplate.mock.calls.find(args => args[0].src.includes('plugin.js'))
+    const options = call[0].options
+    expect(options.baseURL.toString()).toBe('http://localhost:3000/')
+    expect(options.browserBaseURL.toString()).toBe('/test_api')
+  })
+})
+
+describe('empty config', () => {
+  beforeAll(async () => {
+    config.axios = {}
+
+    await setupNuxt(config)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('preset baseURL and browserBaseURL', () => {
+    expect(addTemplate).toBeDefined()
+    const call = addTemplate.mock.calls.find(args => args[0].src.includes('plugin.js'))
+    const options = call[0].options
+    expect(options.baseURL.toString()).toBe('http://localhost:3000/')
+    expect(options.browserBaseURL.toString()).toBe('http://localhost:3000/')
   })
 })
