a016fe768f7ffe661806155d2c09766946494212
T0shik <antonwieslander@hotmail.com>
2020-08-01 19:44:02 +0100

    linting errors

--- test/fixture/api.js
+++ test/fixture/api.js
@@ -1,19 +1,19 @@
 const url = require('url')
 
-function sleep(ms) {
+function sleep (ms) {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}   
+    setTimeout(resolve, ms)
+  })
+}
 
 module.exports = {
   path: '/test_api',
   async handler (req, res) {
-    const query = url.parse(req.url,true).query;
-    if(query && query.delay){
+    const query = url.URL(req.url, true).query
+    if (query && query.delay) {
       await sleep(query.delay)
     }
-    
+
     res.end(JSON.stringify({
       url: req.url,
       method: req.method
