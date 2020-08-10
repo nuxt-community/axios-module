60cfe089f53edf1c1216511fd07202c83f3848ac
Ilya <ilya.sosidka@gmail.com>
2020-08-10 13:10:48 +0300

    docs: installed nuxt docs theme

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  loading: { color: '#00CD81' },
+  i18n: {
+    locales: () => [{
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en'
+  }
+})
