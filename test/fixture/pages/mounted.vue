369639f6bfb54b1d9a33fcda6602b4cae5c8b569
Seb <git@erhardt.net>
2021-10-04 06:22:23 -0700

    typo

--- test/fixture/pages/mounted.vue
+++ test/fixture/pages/mounted.vue
@@ -13,7 +13,7 @@ export default {
   },
 
   async mounted () {
-    // Request with full url becasue we are in JSDom env
+    // Request with full url because we are in JSDom env
     this.res = await this.$axios.$get('http://localhost:3000/api/echo/foo/bar')
   }
 }
