5ddc7c9b69b2d19ea7b7b746a165e255bac2add6
simon <s.tretter@gmail.com>
2021-10-13 09:52:01 +0200

    fix: improve types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,89 +1,131 @@
-import { AxiosError, HAxiosRequestConfig, AxiosRequestConfig, AxiosResponse, AxiosStatic, AxiosConfig } from 'haxios'
-import Vue from 'vue'
-import './vuex'
+import {
+  AxiosError,
+  HAxiosRequestConfig,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosStatic,
+  AxiosConfig,
+  DefaultRequestConfig
+} from 'haxios';
+import Vue from 'vue';
+import './vuex';
 
 interface NuxtAxiosInstance extends AxiosStatic {
   $request<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $get<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $delete<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $head<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $options<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $post<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, data?: INPUT, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    data?: INPUT,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $put<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, data?: any, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    data?: any,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
   $patch<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(url: string, data?: any, config?: CONFIG): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    url: string,
+    data?: any,
+    config?: CONFIG
+  ): Promise<AxiosResponse<RETURN, INPUT, CONFIG>['data']>;
 
-  setBaseURL(baseURL: string): void
-  setHeader(name: string, value?: string | false, scopes?: string | string[]): void
-  setToken(token: string | false, type?: string, scopes?: string | string[]): void
+  setBaseURL(baseURL: string): void;
+  setHeader(name: string, value?: string | false, scopes?: string | string[]): void;
+  setToken(token: string | false, type?: string, scopes?: string | string[]): void;
 
-  onRequest(callback: (config: HAxiosRequestConfig) => void | HAxiosRequestConfig | Promise<HAxiosRequestConfig>): void
+  onRequest(
+    callback: (
+      config: HAxiosRequestConfig
+    ) => void | HAxiosRequestConfig | Promise<HAxiosRequestConfig>
+  ): void;
   onResponse<
     RETURN = any,
     INPUT = any,
-    CONFIG extends HAxiosRequestConfig<INPUT> = HAxiosRequestConfig<INPUT>
-    >(callback: (response: AxiosResponse<RETURN, INPUT, CONFIG>) => void | AxiosResponse<RETURN, INPUT, CONFIG> | Promise<AxiosResponse<RETURN, INPUT, CONFIG>> ): void
-  onError(callback: (error: AxiosError) => any): void
-  onRequestError(callback: (error: AxiosError) => any): void
-  onResponseError(callback: (error: AxiosError) => any): void
+    CONFIG extends HAxiosRequestConfig<INPUT> = DefaultRequestConfig<INPUT>
+    >(
+    callback: (
+      response: AxiosResponse<RETURN, INPUT, CONFIG>
+    ) => void | AxiosResponse<RETURN, INPUT, CONFIG> | Promise<AxiosResponse<RETURN, INPUT, CONFIG>>
+  ): void;
+  onError(callback: (error: AxiosError) => any): void;
+  onRequestError(callback: (error: AxiosError) => any): void;
+  onResponseError(callback: (error: AxiosError) => any): void;
 
-  create(options?: HAxiosRequestConfig): NuxtAxiosInstance
+  create(options?: HAxiosRequestConfig): NuxtAxiosInstance;
 }
 
 interface AxiosOptions {
-  baseURL?: string,
-  browserBaseURL?: string,
-  credentials?: boolean,
-  debug?: boolean,
-  host?: string,
-  prefix?: string,
-  progress?: boolean,
-  proxyHeaders?: boolean,
-  proxyHeadersIgnore?: string[],
-  proxy?: boolean,
-  port?: string | number,
-  retry?: boolean | AxiosConfig['retryConfig'],
-  https?: boolean,
+  baseURL?: string;
+  browserBaseURL?: string;
+  credentials?: boolean;
+  debug?: boolean;
+  host?: string;
+  prefix?: string;
+  progress?: boolean;
+  proxyHeaders?: boolean;
+  proxyHeadersIgnore?: string[];
+  proxy?: boolean;
+  port?: string | number;
+  retry?: boolean | AxiosConfig['retryConfig'];
+  https?: boolean;
   headers?: {
-    common?: Record<string, string>,
-    delete?: Record<string, string>,
-    get?: Record<string, string>,
-    head?: Record<string, string>,
-    post?: Record<string, string>,
-    put?: Record<string, string>,
-    patch?: Record<string, string>,
-  },
+    common?: Record<string, string>;
+    delete?: Record<string, string>;
+    get?: Record<string, string>;
+    head?: Record<string, string>;
+    post?: Record<string, string>;
+    put?: Record<string, string>;
+    patch?: Record<string, string>;
+  };
 }
 
 declare module 'haxios' {
@@ -94,30 +136,30 @@ declare module 'haxios' {
 
 declare module '@nuxt/vue-app' {
   interface Context {
-    $axios: NuxtAxiosInstance
+    $axios: NuxtAxiosInstance;
   }
   interface NuxtAppOptions {
-    $axios: NuxtAxiosInstance
+    $axios: NuxtAxiosInstance;
   }
 }
 
 // Nuxt 2.9+
 declare module '@nuxt/types' {
   interface Context {
-    $axios: NuxtAxiosInstance
+    $axios: NuxtAxiosInstance;
   }
 
   interface NuxtAppOptions {
-    $axios: NuxtAxiosInstance
+    $axios: NuxtAxiosInstance;
   }
 
   interface Configuration {
-    axios?: AxiosOptions
+    axios?: AxiosOptions;
   }
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $axios: NuxtAxiosInstance
+    $axios: NuxtAxiosInstance;
   }
 }
